5149c37fef5855f7248f0e7e082d8d6fd448fda5
John Ma <wangqianyuan88@gmail.com>
2019-03-05 20:46:35 +0800

    '更新华尔街见闻自动收集 可以自动获取当天的快讯'

--- 13/wallstreet.py
+++ 13/wallstreet.py
@@ -10,62 +10,54 @@ import json
 # coding:UTF-8
 import time
 import datetime
+import sys
+import io
+import numpy
 
 # 时间戳转化为时间
-def timestampToTime(now):    # 时间戳
+def timestampToTime(now):  # 时间戳
     now = now
     int(now)
     tl = time.localtime(now)
     # 格式化时间
     format_time = time.strftime("%Y-%m-%d %H:%M:%S", tl)
-    print(format_time)
 
+# 单次去获取数据
 def wallstreet_text_get(timestamp):
-
     textAndTime = []
-
     block = ['global', 'blockchain', 'a_stock', 'us_stock', 'forex', 'commodity']
-
     headers = {'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
                'Accept-Encoding': 'gzip, deflate',
                'Accept-Language': 'zh-CN,zh;q=0.9,en;q=0.8,en-US;q=0.7',
                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36', }
-
     # 上市股票地址
     target_url = 'https://api-prod.wallstreetcn.com/apiv1/content/lives?channel=' + block[
-        0] + '-channel&client=pc&cursor=' + str(timestamp)
-
-    req = requests.get(url=target_url, headers=headers)
-    print(req.status_code)
+        0] + '-channel&client=pc&cursor=' + str(timestamp-1)
+    req = requests.get(url=target_url)
     req.encoding = 'utf-8'
     html = req.text
-
     wallstreet_all_message = json.loads(html)
-
-    # print(wallstreet_all_message['data']['items'])
     if not wallstreet_all_message['data']['items']:
-        print(wallstreet_all_message)
+        print('items没有数据')
     else:
         for item in wallstreet_all_message['data']['items']:
             content_text = item['content_text']
             display_time = item['display_time']
             textAndTime.append(content_text)
             textAndTime.append(display_time)
-            # print(content_text, display_time)
-        # print('OK')
-        # print(len(wallstreet_all_message['data']['items']))
+            # print(display_time)
     return textAndTime
+
 
 # 定时去爬取数据
 def timer(n):
     while True:
-        get_one_day()
+        print(len(get_one_day()))
         time.sleep(n)
 
-#
+# 获取一天的所有信息   成功返回当天的      失败返回空
 def get_one_day():
-
-    textAndTime = []
+    textAndTime_oneDay = []
     # 获取当前时间
     time_now = int(time.time())
     # 转换成localtime
@@ -79,22 +71,30 @@ def get_one_day():
     ts = time.strptime(format_time + ' 00:00:00', "%Y-%m-%d %H:%M:%S")
 
     # 先对获取一次数据，如果最后一位获取的时间比当天开始时间还小就继续获取，否则就继续获取
+    # 再对已经获取的内容进行筛选只是当天的数据
     time.localtime()
-    textAndTime = wallstreet_text_get(timestamp)
-    # print(textAndTime)
-    if textAndTime:
-        intoftext = textAndTime[len(textAndTime)-1]
-        print(type(intoftext))
-        print(type(int(time.mktime(ts))))
-        while(int(intoftext) > int(time.mktime(ts))):
-            textAndTime.extend(wallstreet_text_get(intoftext))
-            intoftext = textAndTime[len(textAndTime)-1]
+    textAndTime_all = wallstreet_text_get(timestamp)
+    if textAndTime_all:
+        intoftext = textAndTime_all[len(textAndTime_all) - 1]
+        while (int(intoftext) > int(time.mktime(ts))):
+            textAndTime_all.extend(wallstreet_text_get(intoftext))
+            intoftext = textAndTime_all[len(textAndTime_all) - 1]
             print(intoftext)
+
+        textAndTime_all_2list = [textAndTime_all[i:i+2] for i in range(0,len(textAndTime_all), 2)]
+
+        for pre in textAndTime_all_2list:
+            if (int(pre[1]) > int(time.mktime(ts))):
+                textAndTime_oneDay.extend(pre)
+        textAndTime_oneDay_2list = [textAndTime_oneDay[i:i + 2] for i in range(0, len(textAndTime_oneDay), 2)]
+        print(textAndTime_oneDay_2list)
+        return textAndTime_oneDay_2list
     else:
-        return textAndTime
+        return textAndTime_all
 
 
 if __name__ == "__main__":
     # 5s
+    sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='gb18030')  # 改变标准输出的默认编码
     timer(1)
 
